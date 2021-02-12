e84bb6325d761fd8de844db134f2728699a21f16
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-02-11 20:51:40 -0500

    orderdetails

--- ElevenFiftySports.Data/OrderDetail.cs
+++ ElevenFiftySports.Data/OrderDetail.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenFiftySports.Data
 {
-    public class OrderDetail
+    public class OrderDetail    
     {
+        [Key]
+        [ForeignKey(nameof(Order))]
+        public int OrderId { get; set; }
+        [Key]
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
     }
 }
