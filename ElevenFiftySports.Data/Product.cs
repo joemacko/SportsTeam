b000f351451c5d87f414a5d1417a2c0138151683
Erik Heath <erikheath8@gmail.com>
2021-02-11 21:35:10 -0500

    Erik's Product Class

--- ElevenFiftySports.Data/Product.cs
+++ ElevenFiftySports.Data/Product.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenFiftySports.Data
 {
+
+    public enum ProductType
+    {
+        Food = 1,
+        Drink
+    }
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "The field {0} must be greater than 0.")]
+        public int UnitCount { get; set; }
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "The field {0} must be greater than 0.00.")]
+        public double ProductPrice { get; set; }
+
+        [Required]
+        public ProductType GetProductType { get; set; }
+
     }
 }
