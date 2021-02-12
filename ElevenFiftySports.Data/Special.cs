0c94d31f2e053ec39ee3ff5fa612e3b511bb02c9
joemacko <joemacko58@gmail.com>
2021-02-11 21:01:46 -0500

    added properties to special class

--- ElevenFiftySports.Data/Special.cs
+++ ElevenFiftySports.Data/Special.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace ElevenFiftySports.Data
 {
     public class Special
     {
+        [Key]
+        public int SpecialId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public DateTime DayOfWeek { get; set; }
+
+        [Required]
+        public double ProductSpecialPrice { get; set; }
     }
 }
