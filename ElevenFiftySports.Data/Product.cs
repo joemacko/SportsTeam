8d36d251aa14815a34f50b2633476c06a7f6e47e
Erik Heath <erikheath8@gmail.com>
2021-02-26 18:10:05 -0500

    added product services classes

--- ElevenFiftySports.Data/Product.cs
+++ ElevenFiftySports.Data/Product.cs
@@ -16,7 +16,6 @@ namespace ElevenFiftySports.Data
     {
         [Key]
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
 
         [Required]
         public string ProductName { get; set; }
@@ -28,34 +27,18 @@ namespace ElevenFiftySports.Data
         public double ProductPrice { get; set; }
 
         [Required]
-<<<<<<< HEAD
-        public ProductType GetProductType { get; set; }
-
-        //add virtual, did not new up (as done in restaurantrater)
-        public virtual List<OrderDetail> OrderDetails { get; set; }
-=======
         public ProductType TypeOfProduct { get; set; }
+
         public virtual List<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
->>>>>>> 8082adb84ba2b850646cb959da84d760ba7b1c31
         public virtual List<Special> ProductSpecials { get; set; } = new List<Special>();
 
-        public bool IsSpecial //we might not need this because the logic of productspecials.count > 0 could just be used in the service
+        public bool IsSpecial
         {
             get
             {
-<<<<<<< HEAD
-                foreach (Special special in ProductSpecials)
-                {
-                    if (ProductId == special.ProductId)
-                    {
-                        return true;
-                    }
-                }
-=======
                 if (ProductSpecials.Count > 0) //the ProductSpecials list will only have specials on it if the product IS a special
                     return true;
 
->>>>>>> 8082adb84ba2b850646cb959da84d760ba7b1c31
                 return false;
             }
         }
