24d96fb52bbbcc4117dd0e8a198016c3d6062910
Erik Heath <erikheath8@gmail.com>
2021-03-03 14:07:49 -0500

    product controller added

--- ElevenFiftySports/Controllers/ProductController.cs
+++ ElevenFiftySports/Controllers/ProductController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+using ElevenFiftySports.Services;
 
 namespace ElevenFiftySports.Controllers
 {
+    [System.Web.Http.Authorize]
     public class ProductController : Controller
     {
         // GET: Product
+        public IHttpActionResult Get()
+        {
+            ProductService productService = CreateProductService();
+            var products = productService.GetProducts();
+            return Ok(products);
+        }
+
+
+
         public ActionResult Index()
         {
             return View();
